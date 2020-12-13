e1df82d79663c9b6a465128ac98c17209818800e
kencormican <ken.cormican@bt.com>
2020-12-13 13:54:33 +0000

    Item Model applied and migrated to django

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Item(models.Model):
+    # form validation max 50 characters, required
+    name = models.CharField(max_length=50, null=False, blank=False)
+    # form validation, required and default value
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.name
