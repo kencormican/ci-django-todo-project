9272575bf69c9f1d968bc0993619651c9254bf0a
kencormican <ken.cormican@bt.com>
2020-12-19 14:18:12 +0000

    Django testing & Testing forms lesson code added

---
+++ todo/test_views.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+
+# Basic Test validation
+class TestDjango(TestCase):
+
+    def test_this_thing_works(self):
+        self.assertEqual(1, 1)                      # If 1 is equal to 1
