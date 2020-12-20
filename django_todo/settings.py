0029682bd561fe282f3e52e0234789ae5c0bb63e
kencormican <ken.cormican@bt.com>
2020-12-20 19:29:07 +0000

    postgres dependencies added and wired to heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +75,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://ulnogglirnyixv:532bccd4abc232f99b85f7e6746c1bd0bd2a3e28abb816a740d263c77fe58038@ec2-3-248-4-172.eu-west-1.compute.amazonaws.com:5432/d75kvo1cvgna8m')
 }
 
 
